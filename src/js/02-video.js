0d05e454a7f6ba2d6acd81e43facb1862ffdcce1
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2021-11-22 19:18:03 +0200

    task02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+console.log(localStorage.getItem('videoplayer-current-time'));
 
+player.on('play', function (event) {
+    const currentTime = localStorage.getItem('videoplayer-current-time')
+    console.log(currentTime);
+    if (currentTime == null) {
+        return console.log('played the video!');
+    }
+    player.setCurrentTime(currentTime)
+});
+
+// player.getVideoTitle().then(function (title) {
+//     console.log('title:', title);
+// });
+
+player.on('timeupdate', saveTime)
+
+function saveTime(event) {
+    const currentTime = event.seconds
+    localStorage.setItem('videoplayer-current-time', `${currentTime}`)
+}
