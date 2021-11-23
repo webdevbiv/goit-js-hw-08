eea41e598a19d9b6a4b3c41fc97be19775d06ca9
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2021-11-24 00:28:47 +0200

    video fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,16 +2,22 @@
 const _ = require('lodash');
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
+
 console.log(localStorage.getItem('videoplayer-current-time'));
 
-player.on('play', function (event) {
-    const currentTime = localStorage.getItem('videoplayer-current-time')
-    console.log(currentTime);
-    if (currentTime == null) {
-        return console.log('played the video!');
-    }
-    player.setCurrentTime(currentTime)
-});
+const savedTime = localStorage.getItem('videoplayer-current-time')
+if (savedTime !== null) {
+    player.setCurrentTime(savedTime)
+}
+
+// player.on('play', function (event) {
+//     const currentTime = localStorage.getItem('videoplayer-current-time')
+//     console.log(currentTime);
+//     if (currentTime == null) {
+//         return console.log('played the video!');
+//     }
+//     player.setCurrentTime(currentTime)
+// });
 
 // player.getVideoTitle().then(function (title) {
 //     console.log('title:', title);
